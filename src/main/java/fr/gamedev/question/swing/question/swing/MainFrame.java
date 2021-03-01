71340a662521d9e38b0675256df2d89699246b0b
djer13 <djer13@gmail.com>
2021-03-01 18:35:09 +0100

    add minimal implementation of custom close button

--- src/main/java/fr/gamedev/question/swing/question/swing/MainFrame.java
+++ src/main/java/fr/gamedev/question/swing/question/swing/MainFrame.java
@@ -1,5 +1,9 @@
 package fr.gamedev.question.swing.question.swing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -7,7 +11,7 @@ import javax.swing.JFrame;
  * 
  * @author djer13
  */
-public class MainFrame extends JFrame {
+public class MainFrame extends JFrame implements ActionListener {
     /** serialVersionUID */
     private static final long serialVersionUID = 2827577217081048727L;
 
@@ -16,9 +20,19 @@ public class MainFrame extends JFrame {
     public MainFrame() {
         exit = new JButton("quitter ?");//create button  
         exit.setBounds(130, 100, 100, 40);
+        exit.setActionCommand("quit");
+        exit.addActionListener(this);
 
         super.add(exit);//adding button on frame  
         super.setSize(400, 500);
         super.setLayout(null);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("quit".equals(e.getActionCommand())) {
+            super.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+        }
+    }
+
 }
